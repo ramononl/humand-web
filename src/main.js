abc835032dd232ba706d37704d3a22dac3b6294f
ramononl <>
2019-11-01 15:46:53 +0100

    add loading progress bar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import NProgress from "nprogress";
+import "~/assets/css/custom_nprogress.css";
 require("typeface-lato");
 
 import DefaultLayout from "~/layouts/Default.vue";
@@ -12,6 +14,8 @@ import IconBase from "~/components/common/IconBase.vue";
 import IconUser from "~/components/icons/IconUser.vue";
 import IconClose from "~/components/icons/IconClose.vue";
 import IconVerified from "~/components/icons/IconVerified.vue";
+import IconBack from "~/components/icons/IconBack.vue";
+import IconArrowRight from "~/components/icons/IconArrowRight.vue";
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
@@ -25,7 +29,23 @@ export default function(Vue, { router, head, isClient }) {
   Vue.component("IconUser", IconUser);
   Vue.component("IconClose", IconClose);
   Vue.component("IconVerified", IconVerified);
+  Vue.component("IconBack", IconBack);
+  Vue.component("IconArrowRight", IconArrowRight);
 
   // Add attributes to BODY tag
   head.bodyAttrs = { class: "bg-green-050 text-gray-900 font-sans antialiased relative min-h-screen pb-8" };
+
+  // Configure NProgress
+  NProgress.configure({ showSpinner: false });
+
+  router.beforeEach((to, from, next) => {
+    if (from.name !== null) {
+      NProgress.start();
+    }
+    next();
+  });
+
+  router.afterEach((to, from) => {
+    NProgress.done();
+  });
 }
