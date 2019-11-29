796003c43fc8199d36e551976017878558a074d8
ramononl <48051529+ramononl@users.noreply.github.com>
2019-11-29 22:34:08 +0100

    add hash navigation

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -2,31 +2,52 @@
   <Layout>
     <ContainerConstrained>
       <div class="mb-6 px-6 sm:px-8 md:px-12">
-        <h1>Settings</h1>
-        <p class="mt-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <h1>Allison Morrison</h1>
+        <p class="mt-0">All your donations, news and settings in one place.</p>
       </div>
       <div class="bg-white shadow-lg rounded-lg overflow-hidden">
         <div class="md:flex">
           <div class="md:w-8/12 px-6 sm:px-8 md:px-12 py-10 flex-shrink-0">
-            <ViewDashboard v-if="view === 'dashboard'"/>
-            <ViewInbox v-if="view === 'inbox'"/>
-            <ViewDonations v-if="view === 'donations'"/>
-            <ViewSettings v-if="view === 'settings'"/>
-            <ViewPayment v-if="view === 'payment'"/>
-            <ViewAnalytics v-if="view === 'analytics'"/>
-            <ViewNotifications v-if="view === 'notifications'"/>
+            <ViewDashboard v-if="view === '#dashboard'"/>
+            <ViewInbox v-if="view === '#inbox'"/>
+            <ViewDonations v-if="view === '#donations'"/>
+            <ViewSettings v-if="view === '#settings'"/>
+            <ViewPayment v-if="view === '#payment'"/>
+            <ViewAnalytics v-if="view === '#analytics'"/>
+            <ViewNotifications v-if="view === '#notifications'"/>
           </div>
           <div class="md:w-4/12 px-6 sm:px-8 py-10 flex-shrink-0 md:border-l border-gray-300">
             <div class="border border-gray-500 rounded-lg overflow-hidden">
-              <button @click="changeView('dashboard')" :class="[view === 'dashboard' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">Dashboard</button>
-              <button @click="changeView('inbox')" :class="[view === 'inbox' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">Inbox</button>
-              <button @click="changeView('donations')" :class="[view === 'donations' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none">Donations</button>
+              <button @click="changeView('dashboard')" :class="[view === '#dashboard' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+                <IconBase :class="[view === '#dashboard' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconDashboard/></IconBase>
+                <span class="ml-2 font-semibold">Dashboard</span>
+              </button>
+              <button @click="changeView('inbox')" :class="[view === '#inbox' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+                <IconBase :class="[view === '#inbox' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconInbox/></IconBase>
+                <span class="ml-2 font-semibold">Inbox</span>
+              </button>
+              <button @click="changeView('donations')" :class="[view === '#donations' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none">
+                <IconBase :class="[view === '#donations' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconDonations/></IconBase>
+                <span class="ml-2 font-semibold">Donations</span>
+              </button>
             </div>
             <div class="mt-6 border border-gray-500 rounded-lg overflow-hidden">
-              <button @click="changeView('settings')" :class="[view === 'settings' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">Settings</button>
-              <button @click="changeView('payment')" :class="[view === 'payment' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">Payment</button>
-              <button @click="changeView('analytics')" :class="[view === 'analytics' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">Analytics</button>
-              <button @click="changeView('notifications')" :class="[view === 'notifications' ? 'text-green-100 bg-green-500' : 'text-gray-600']" class="block w-full px-4 py-2 text-left text-sm focus:outline-none">Notifications</button>
+              <button @click="changeView('settings')" :class="[view === '#settings' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+                <IconBase :class="[view === '#settings' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconSettings/></IconBase>
+                <span class="ml-2 font-semibold">Settings</span>
+              </button>
+              <button @click="changeView('payment')" :class="[view === '#payment' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+                <IconBase :class="[view === '#payment' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconPayment/></IconBase>
+                <span class="ml-2 font-semibold">Payment</span>
+              </button>
+              <button @click="changeView('analytics')" :class="[view === '#analytics' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+                <IconBase :class="[view === '#analytics' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconAnalytics/></IconBase>
+                <span class="ml-2 font-semibold">Analytics</span>
+              </button>
+              <button @click="changeView('notifications')" :class="[view === '#notifications' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none">
+                <IconBase :class="[view === '#notifications' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconNotifications/></IconBase>
+                <span class="ml-2 font-semibold">Notifications</span>
+              </button>
             </div>
           </div>
         </div>
@@ -51,14 +72,24 @@ export default {
   components: {
     ViewDashboard, ViewInbox, ViewDonations, ViewSettings, ViewPayment, ViewAnalytics, ViewNotifications
   },
-  data() {
-    return {
-      view: 'dashboard'
+  computed: {
+    view() {
+      return this.$route.hash;
     }
   },
   methods: {
     changeView(selectedView) {
       this.view = selectedView;
+      window.location.hash = "#" + selectedView;
+    }
+  },
+  watch: {
+    view() {
+      if (location.hash) {
+        this.view = window.location.hash;
+      } else {
+        this.view = "#dashboard";
+      }
     }
   }
 }
