5aea1a0dcf0214f72388951196ed84165504b01f
ramononl <48051529+ramononl@users.noreply.github.com>
2019-11-29 19:50:18 +0100

    add margin top

--- src/components/ui/ProjectTag.vue
+++ src/components/ui/ProjectTag.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ml-1 px-3 py-1 inline-block bg-gray-300 rounded-full uppercase font-bold tracking-wider text-gray-00 text-xxs">
+  <div class="mt-2 ml-1 px-3 py-1 inline-block bg-gray-300 rounded-full uppercase font-bold tracking-wider text-gray-00 text-xxs">
     <slot/>
   </div>
 </template>
