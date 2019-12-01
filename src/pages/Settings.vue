5f2061834ac82e1b18821f5920d8fe1c8fac091d
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-01 14:21:07 +0100

    fix hash routing

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -18,36 +18,36 @@
           </div>
           <div class="md:w-4/12 px-6 sm:px-8 py-10 flex-shrink-0 md:border-l border-gray-300">
             <div class="border border-gray-500 rounded-lg overflow-hidden">
-              <button @click="changeView('dashboard')" :class="[view === '#dashboard' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+              <a href="#dashboard" :class="[view === '#dashboard' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
                 <IconBase :class="[view === '#dashboard' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconDashboard/></IconBase>
                 <span class="ml-2 font-semibold">Dashboard</span>
-              </button>
-              <button @click="changeView('inbox')" :class="[view === '#inbox' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+              </a>
+              <a href="#inbox" :class="[view === '#inbox' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
                 <IconBase :class="[view === '#inbox' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconInbox/></IconBase>
                 <span class="ml-2 font-semibold">Inbox</span>
-              </button>
-              <button @click="changeView('donations')" :class="[view === '#donations' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none">
+              </a>
+              <a href="#donations" :class="[view === '#donations' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none">
                 <IconBase :class="[view === '#donations' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconDonations/></IconBase>
                 <span class="ml-2 font-semibold">Donations</span>
-              </button>
+              </a>
             </div>
             <div class="mt-6 border border-gray-500 rounded-lg overflow-hidden">
-              <button @click="changeView('settings')" :class="[view === '#settings' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+              <a href="#settings" :class="[view === '#settings' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
                 <IconBase :class="[view === '#settings' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconSettings/></IconBase>
                 <span class="ml-2 font-semibold">Settings</span>
-              </button>
-              <button @click="changeView('payment')" :class="[view === '#payment' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+              </a>
+              <a href="#payment" :class="[view === '#payment' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
                 <IconBase :class="[view === '#payment' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconPayment/></IconBase>
                 <span class="ml-2 font-semibold">Payment</span>
-              </button>
-              <button @click="changeView('analytics')" :class="[view === '#analytics' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
+              </a>
+              <a href="#analytics" :class="[view === '#analytics' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none border-b border-gray-500">
                 <IconBase :class="[view === '#analytics' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconAnalytics/></IconBase>
                 <span class="ml-2 font-semibold">Analytics</span>
-              </button>
-              <button @click="changeView('notifications')" :class="[view === '#notifications' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none">
+              </a>
+              <a href="#notifications" :class="[view === '#notifications' ? 'active text-green-100 bg-green-500' : 'text-gray-600']" class="flex items-center w-full pl-3 pr-4 py-2 text-left text-sm focus:outline-none">
                 <IconBase :class="[view === '#notifications' ? 'text-green-700' : 'text-gray-500']" :iconClasses="['w-6 h-6']"><IconNotifications/></IconBase>
                 <span class="ml-2 font-semibold">Notifications</span>
-              </button>
+              </a>
             </div>
           </div>
         </div>
@@ -74,22 +74,17 @@ export default {
   },
   computed: {
     view() {
-      return this.$route.hash;
+      if (this.$route.hash) {
+        return this.$route.hash;
+      } else {
+        return "#dashboard";
+      }
     }
   },
   methods: {
     changeView(selectedView) {
       this.view = selectedView;
-      window.location.hash = "#" + selectedView;
-    }
-  },
-  watch: {
-    view() {
-      if (location.hash) {
-        this.view = window.location.hash;
-      } else {
-        this.view = "#dashboard";
-      }
+      window.location.hash = selectedView;
     }
   }
 }
