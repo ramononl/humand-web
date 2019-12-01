f0012bd6a80aace8ba2a48ca07e70db993c183e6
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-01 13:19:31 +0100

    test build

--- src/main.js
+++ src/main.js
@@ -67,7 +67,7 @@ export default function(Vue, { router, head, isClient, appOptions }) {
   // Configure Vuex
   Vue.use(Vuex);
 
-  const plugins = [];
+  let plugins = [];
   if (process.isClient) {
     plugins = [createPersistedState()];
   }
