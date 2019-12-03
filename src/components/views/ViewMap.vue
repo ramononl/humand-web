dd9ffc12bca022fa205a98d7f5ba839322ea5081
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-03 17:03:50 +0100

    add map

--- src/components/views/ViewMap.vue
+++ src/components/views/ViewMap.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h2>Map</h2>
+    <h2>Project Map (Beta)</h2>
+    <p class="mt-1 text-gray-600">Search for projects based on their location on the map below.</p>
+    <div class="mt-4 map-responsive">
+      <iframe src="https://www.google.com/maps/d/embed?mid=1fZciR5pJM-jf2vytwYvOvd2tyysjsNvx&hl=de" width="640" height="480"></iframe>
+    </div>
   </div>
 </template>
 
@@ -9,6 +13,18 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+.map-responsive{
+  overflow:hidden;
+  padding-bottom:56.25%;
+  position:relative;
+  height:0;
+}
+.map-responsive iframe{
+  left:0;
+  top:0;
+  height:100%;
+  width:100%;
+  position:absolute;
+}
 </style>
