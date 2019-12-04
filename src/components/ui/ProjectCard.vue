bd7524da82a6cdb6007d0671c7504773fc7cb70f
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-04 10:04:33 +0100

    add image filter

--- src/components/ui/ProjectCard.vue
+++ src/components/ui/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-5 px-2 max-w-80">
     <g-link :to="cardData.path" class="group">
-      <g-image :src="cardData.image" :alt="cardData.title" class="rounded-lg shadow-md group-hover:opacity-75 transition-all"/>
+      <g-image :src="cardData.image" :alt="cardData.title" class="rounded-lg shadow-md group-hover:opacity-75 transition-all" style="filter: grayscale(15%) contrast(80%) brightness(100%);"/>
       <div class="relative px-4 -mt-16 transition-all">
         <div class="bg-white rounded-lg px-4 py-4 shadow-lg h-32 flex flex-col justify-between">
           <div>
