806204678c906455f2f84d1a8e10f8d6ef1b5a05
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-05 14:48:40 +0100

    add color

--- src/components/icons/IconAnalytics.vue
+++ src/components/icons/IconAnalytics.vue
@@ -1,7 +1,7 @@
 <template>
   <g>
     <circle cx="12" cy="12" r="10"></circle>
-    <path class="secondary" d="M12.88 8.88a3 3 0 1 1 4.24 4.24l-4.41 4.42a1 1 0 0 1-1.42 0l-4.41-4.42a3 3 0 1 1 4.24-4.24l.88.88.88-.88z"></path>
+    <path class="secondary fill-current" d="M12.88 8.88a3 3 0 1 1 4.24 4.24l-4.41 4.42a1 1 0 0 1-1.42 0l-4.41-4.42a3 3 0 1 1 4.24-4.24l.88.88.88-.88z"></path>
   </g>
 </template>
 
